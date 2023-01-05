05ebb5149a7badf9cd320bebb59f3a945a229bda
parvinaliyevv <aliyevv_p@outlook.com>
2023-01-05 13:44:13 +0400

    account controller test

---
+++ Tests/eShop.xUnitTest/Controllers/AccountControllerTest.cs
@@ -0,0 +1,72 @@
+﻿namespace eShop.xUnitTest.Controllers;
+
+public class AccountControllerTest
+{
+    [Fact]
+    public async void SignInBadModelRequest()
+    {
+        // Arrange
+        
+        var mapperMock = new Mock<IMapper>();
+        var userManagerMock = new Mock<IUserManager>();
+        var tokenGeneratorMock = new Mock<ITokenGeneratorService>();
+
+        var dtoParam = new SignInUserDto() { Email = "johndoegmail.com", Password = "JohnDoeSecret" };
+
+        var controller = new AccountController(userManagerMock.Object, tokenGeneratorMock.Object, mapperMock.Object);
+        controller.ModelState.AddModelError("Invalid email address", "email address contain '@' and '.' !");
+
+        // Act
+
+        var result = await controller.SignIn(dtoParam);
+
+        // Assert
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    [Fact]
+    public async void SignInNotFoundRequest()
+    {
+        // Arrange
+
+        var mapperMock = new Mock<IMapper>();
+        var userManagerMock = new Mock<IUserManager>();
+        var tokenGeneratorMock = new Mock<ITokenGeneratorService>();
+
+        var dtoParam = new SignInUserDto() { Email = "johndoe@gmail.com", Password = "JohnDoeSecret" };
+
+        var controller = new AccountController(userManagerMock.Object, tokenGeneratorMock.Object, mapperMock.Object);
+
+        // Act
+
+        var result = await controller.SignIn(dtoParam);
+
+        // Assert
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public async void SignUpBadModelRequest()
+    {
+        // Arrange
+
+        var mapperMock = new Mock<IMapper>();
+        var userManagerMock = new Mock<IUserManager>();
+        var tokenGeneratorMock = new Mock<ITokenGeneratorService>();
+
+        var dtoParam = new SignUpUserDto() { Email = "johndoegmail.com", Password = "JohnDoeSecret" };
+
+        var controller = new AccountController(userManagerMock.Object, tokenGeneratorMock.Object, mapperMock.Object);
+        controller.ModelState.AddModelError("Invalid email address", "email address contain '@' and '.' !");
+
+        // Act
+
+        var result = await controller.SignUp(dtoParam);
+
+        // Assert
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+}
---
+++ Tests/eShop.xUnitTest/Import.cs
@@ -0,0 +1,9 @@
+
+global using eShop.API.Controllers;
+global using eShop.Application.Dtos;
+global using eShop.Application.Services;
+
+global using Moq;
+global using Xunit;
+global using AutoMapper;
+global using Microsoft.AspNetCore.Mvc;
